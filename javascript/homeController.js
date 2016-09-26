97701a083c6088fe9725835e3d5bc8e6efff0214
Matthew Stromberg <mstromb1@binghamton.edu>
2016-09-26 16:34:15 -0400

    Needs content and potentially more styling, but is functional now.

--- javascript/homeController.js
+++ javascript/homeController.js
@@ -3,4 +3,13 @@ app.controller('homeController', function ($scope, $http) {
         .then(function (response) {
             $scope.githubObject = response.data;
         });
+    $scope.last = "";
+
+    $scope.myFunc = function (id) {
+        angular.element("#" + id).addClass("activeClass");
+        if ($scope.last != "") {
+            angular.element("#" + $scope.last).removeClass("activeClass");
+        }
+        $scope.last = id;
+    }
 });
