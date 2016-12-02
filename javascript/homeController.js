d43ed3aed49dfdbead65008063420981a14587a2
Matthew Stromberg <mstromb1@binghamton.edu>
2016-12-02 11:49:36 -0500

    Added some things

--- javascript/homeController.js
+++ javascript/homeController.js
@@ -7,10 +7,16 @@ app.controller('homeController', function ($scope, $http) {
     $http.get("https://api.bitbucket.org/2.0/repositories/mstromb1")
         .then(function (response) {
             $scope.bitbucketObject = response.data;
-            angular.forEach($scope.bitbucketObject, function (item) {
-                console.log(item);
-            });
+            /*            angular.forEach($scope.bitbucketObject, function (item) {
+                            console.log(item);
+                        });
+                        */
+        });
 
+    $http.get("/projects.json")
+        .then(function (response) {
+            $scope.projectsObject = response.data;
+            console.log($scope.projectsObject);
         });
     $scope.last = "";
 
